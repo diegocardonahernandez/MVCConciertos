a30b36075e7c512512f14a8686e9511d91ce2cbe
diegocardonahernandez <diego.cardona@tajamar365.com>
2026-06-10 10:53:07 +0200

    Add project files.

---
+++ MVCConciertos/Controllers/ConciertosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCConciertos.Services;
+
+namespace MVCConciertos.Controllers
+{
+    public class ConciertosController : Controller
+    {
+        private readonly ServiceConciertos _service;
+
+        public ConciertosController(ServiceConciertos service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var eventos = await _service.GetEventosAsync();
+            return View(eventos);
+        }
+
+        public async Task<IActionResult> Categorias()
+        {
+            var categorias = await _service.GetCategoriasAsync();
+            return View(categorias);
+        }
+
+        public async Task<IActionResult> EventosCategoria(int idCategoria)
+        {
+            var eventos = await _service.GetEventosByCategoriaAsync(idCategoria);
+            ViewData["ID_CATEGORIA"] = idCategoria;
+            return View("Index", eventos); // Podemos reusar la vista Index si usa el mismo modelo
+        }
+    }
+}
---
+++ MVCConciertos/Services/ServiceConciertos.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using MVCConciertos.Models; 
+
+namespace MVCConciertos.Services
+{
+    public class ServiceConciertos
+    {
+        private MediaTypeWithQualityHeaderValue header;
+        private string UrlApi;
+
+        public ServiceConciertos(IConfiguration configuration)
+        {
+            string? configUrl = configuration.GetValue<string>("ApiUrls:ApiEventos");
+            
+            this.UrlApi = !string.IsNullOrEmpty(configUrl) 
+                ? configUrl 
+                : "https://2t952p4yac.execute-api.us-east-1.amazonaws.com/Prod/";
+                
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T?> CallApiAsync<T>(string request)
+        {
+            // Medida extra de seguridad
+            if (string.IsNullOrEmpty(this.UrlApi))
+            {
+                throw new InvalidOperationException("La URL de la API no está configurada.");
+            }
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T? data = await response.Content.ReadFromJsonAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Evento>?> GetEventosAsync()
+        {
+            string request = "api/Eventos";
+            return await this.CallApiAsync<List<Evento>>(request);
+        }
+
+        public async Task<List<CategoriaEvento>?> GetCategoriasAsync()
+        {
+            string request = "api/Eventos/Categorias";
+            return await this.CallApiAsync<List<CategoriaEvento>>(request);
+        }
+
+        public async Task<List<Evento>?> GetEventosByCategoriaAsync(int idCategoria)
+        {
+            string request = $"api/Eventos/Categoria/{idCategoria}";
+            return await this.CallApiAsync<List<Evento>>(request);
+        }
+    }
+}
